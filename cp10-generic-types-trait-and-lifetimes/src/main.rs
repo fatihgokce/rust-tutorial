92a5fa2e3f342a2caf5bd7d543dd036144823efb
ekim <ekim@fatihs-MacBook-Air.local>
2019-03-27 12:43:16 +0300

    -chapter 10 ,10.2 Trait

--- cp10-generic-types-trait-and-lifetimes/src/main.rs
+++ cp10-generic-types-trait-and-lifetimes/src/main.rs
@@ -19,6 +19,62 @@ impl<T, U> Point2<T, U> {
         }
     }
 }
+//10.2 trait
+trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+struct NewsArticle {
+    headline: String,
+    location: String,
+    author: String,
+    content: String,
+}
+
+// impl Summary for NewsArticle {
+//     fn summarize(&self) -> String {
+//         format!("{}, by {} ({})", self.headline, self.author, self.location)
+//     }
+// }
+//for default implemantion
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+struct Tweet {
+    username: String,
+    content: String,
+    reply: bool,
+    retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+// //Trait as arguments
+fn notify(item:& impl Summary) {
+    println!("Breaking news! {}", item.summarize_author());
+}
+fn notify2<T: Summary>(item: &T) {
+    println!("2Breaking news! {}", item.summarize_author());
+}
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
 fn main() {
    let number_list = vec![34, 50, 25, 100, 65];
    let f=Point2{x:1,y:2.1};
@@ -32,6 +88,54 @@ fn main() {
     let p2 = Point2 { x: "Hello", y: 'c'};
     let p3 = p1.mixup(p2);
     println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    //10.2 Trait
+    let article = NewsArticle {
+    headline: String::from("Penguins win the Stanley Cup Championship!"),
+    location: String::from("Pittsburgh, PA, USA"),
+    author: String::from("Iceburgh"),
+    content: String::from("The Pittsburgh Penguins once again are the best
+    hockey team in the NHL."),
+    };
+    let tweet = Tweet {
+    username: String::from("horse_ebooks"),
+    content: String::from("of course, as you probably already know, people"),
+    reply: false,
+    retweet: false,
+    };
+    
+    println!("New article available! {}", article.summarize());
+     //Trait as arguments
+    notify(&tweet);
+    notify(&article);
+    //Trait Bounds
+    notify2(&tweet);
+    /*
+    fn notify(item1: impl Summary, item2: impl Summary) {
+    //same
+    fn notify<T: Summary>(item1: T, item2: T) { 
+    */
+    // Specify nulttipe traits with +
+    /*
+    fn notify(item: impl Summary + Display) {
+        //with generic
+    fn notify<T: Summary + Display>(item: T) {
+     */
+    // where clauses for clearer code
+    /*
+    fn some_function<T: Display + Clone, U: Clone + Debug>(t: T, u: U) -> i32 {
+    //we can use a where clause, like this:
+    fn some_function<T, U>(t: T, u: U) -> i32
+    where T: Display + Clone,
+          U: Clone + Debug
+    {
+    */
+    //Returning Traits
+    /*
+    This signature says, “I’m going to return something that implements the Summary trait, but I’m not going to tell you the exact type.” 
+    */
+    let rt=returns_summarizable();
+    println!("{}",rt.summarize());
+
 }
 //&Vec<i32>
 fn largest_i32(list: &[i32]) -> i32 {
