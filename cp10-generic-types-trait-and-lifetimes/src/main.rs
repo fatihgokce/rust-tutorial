d49f5e4de9907cfb72b6687dbd54aed9145ada46
ekim <ekim@fatihs-MacBook-Air.local>
2019-03-28 12:10:18 +0300

    chapter 10.3 Validating References with Lifetimes

--- cp10-generic-types-trait-and-lifetimes/src/main.rs
+++ cp10-generic-types-trait-and-lifetimes/src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::Display;
 struct Point<T>{
     x:T,
     y:T,
@@ -135,7 +136,89 @@ fn main() {
     */
     let rt=returns_summarizable();
     println!("{}",rt.summarize());
+    //10-03 validating refereces with lifetimes
+    /*
+    {
+        let r;                // ---------+-- 'a
+                          //          |
+        {                     //          |
+            let x = 5;        // -+-- 'b  |
+            r = &x;           //  |       |
+        }                     // -+       |
+                              //          |
+        println!("r: {}", r); //          |
+    }                         //----------+ 
+    */
+    //Generic Lifetimes in Functions
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    //let ref s3 = &string1; both same
+    //let s3: &String = &string1;
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+    //Lifetime Annotation Syntax
+    /*the names of lifetime parameters must start with an apostrophe (') and are usually all lowercase and very short
+    &i32        // a reference
+    &'a i32     // a reference with an explicit lifetime
+    &'a mut i32 // a mutable reference with an explicit lifetime
+    */
+    //Thinking in Terms of Lifetimes
+    
+    //Lifetime Annotations in Struct Definitions
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
+    //Lifetime Elision
+    //The Static Lifetime
+    /*
+    'static, which denotes the entire duration of the program
+    The text of this string is stored directly in the binary of your program, which is always available. Therefore, the lifetime of all string literals is 'static.
+    */
+   let s: &'static str = "I have a static lifetime.";
+   //Generic Type Parameters, Trait Bounds, and Lifetimes Together
 
+    
+
+}
+////Generic Type Parameters, Trait Bounds, and Lifetimes Together
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+////Lifetime Annotations in Struct Definitions
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+//Generic Lifetimes in Functions
+// fn longest(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+/*
+The function signature now tells Rust that for some lifetime 'a, the function takes two parameters, both of which are string slices that live at least as long as lifetime 'a
+*/
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
 //&Vec<i32>
 fn largest_i32(list: &[i32]) -> i32 {
